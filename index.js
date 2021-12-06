77c543ee1ca6fe2e4c60bb48781cc76bc3f6546e
jean-benoit daille <68898672+jeanbenoitdaille@users.noreply.github.com>
2021-12-06 12:55:13 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+let prenoms = ["Pierre", "Paul", "Marie"];
+     
+let premier = prenoms[0];
+let dernier = prenoms[prenoms.length - 1];
+let deux_premiers = prenoms.slice(0, 2);
+let deux_derniers = prenoms.slice(1, 3);
